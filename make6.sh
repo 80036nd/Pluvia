d2cf8f754cc70594fadb3d1abf9d408633435bd8
80036nd <40246027+Nick80036nd@users.noreply.github.com>
2019-10-01 17:33:27 -0400

    Update and rename make704.sh to make6.sh

---
+++ make6.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Documents/RetroSn0w/
+./make_ipsw.sh ~/Downloads/iPhone3\,1_6.0_10A403_Restore.ipsw
--- make704.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ~/Documents/RetroSn0w/
-./make_ipsw.sh ~/Downloads/iPhone3\,1_7.0.4_11B554a_Restore.ipsw
