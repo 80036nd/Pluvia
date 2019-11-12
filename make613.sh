2995b3717440272f806e715b0cb8e2492a170439
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-12 17:45:16 -0500

    Update make613.sh

--- make613.sh
+++ make613.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Documents/RetroSn0w/
-./make_ipsw.sh ~/Downloads/iPhone3\,1_6.1.3_10B329_Restore.ipsw
+./make_ipsw.sh ~/Downloads/iPhone3\,1_6.1_10B144_Restore.ipsw
 killall Terminal
